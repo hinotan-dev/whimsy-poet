5848572186690800a3a69ade104834341340e4c3
hinotan-dev <hinotan@gmail.com>
2025-06-09 16:18:04 +0900

    Update Footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,11 @@ export default function Footer() {
   return (
     <footer>
       <p>
-        Made with ♥️ by Ellen Zheng 
+        Made with ♥️ by Ellen Zheng&nbsp;
         <br className="hide-tablet-up" />
-        &copy; WhimsyPoet, {new Date().getFullYear()}
+        &copy; WhimsyPoet, {new Date().getFullYear()}.&nbsp;
+        <br className="hide-tablet-up" />
+        Illustrations generated using AI tools.
       </p>
     </footer>
   )
